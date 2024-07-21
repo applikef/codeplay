3228d82025bbbdb6b72debe3964905a3ed1551e4
netta <nettashani@gmail.com>
2024-07-21 12:11:44 +0300

    arrow of pencil is back

--- src/model/KDPencil.tsx
+++ src/model/KDPencil.tsx
@@ -1,16 +1,5 @@
-export const PENCIL_IMAGE = "./resources/pencil128 with arrow.png";
+export const PENCIL_IMAGE = "./resources/pencil128.png";
 export const PENCIL_IMAGE_MAX_DIMENSION = 128;
-
-export const DEFAULT_PENCIL: KDPencil = {
-  x: 100,
-  y: 100,
-  penX: 162,
-  penY: 216,
-  stroke: "#0000ff",
-  angle: 0,
-  rotate: 0
-}
-
 export const DEFAULT_PENCIL_PEN_DELTA_X = 62;
 export const DEFAULT_PENCIL_PEN_DELTA_Y = 116;
 
@@ -22,4 +11,14 @@ export interface KDPencil {
   stroke: string;
   angle: number;
   rotate: number;
+}
+
+export const DEFAULT_PENCIL: KDPencil = {
+  x: 100,
+  y: 100,
+  penX: 162,
+  penY: 216,
+  stroke: "#0000ff",
+  angle: 0,
+  rotate: 0
 }
